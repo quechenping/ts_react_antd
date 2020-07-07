a631e1a0b6f7f0885b4d774cb33035bd1dcb9124
quecp <que_chenping@163.com>
2020-07-07 13:57:01 +0800

    test del hush

--- webpack-react--master/src/components/about.tsx
+++ webpack-react--master/src/components/about.tsx
@@ -5,7 +5,7 @@ import { hot } from "react-hot-loader";
 const About = hot(module)(() => {
   return (
     <div id="div2">
-      hello world hush
+      hello world
       <img src={oimg}></img>
     </div>
   );
